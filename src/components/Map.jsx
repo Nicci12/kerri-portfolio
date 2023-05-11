b507ff9fedb6c0ec9ff4fa72052a3f1a57b90fc4
Your Name <ndimant@hotmail.com>
2023-05-11 21:09:34 +0300

    changing maps

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -16,11 +16,11 @@ const Map = () => {
   };
   return (
     <>
-      {" "}
+    
       <div className="map">
         <div className="embed-responsive embed-responsive-21by9">
           <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d18592.275786665006!2d28.058212432499097!3d-26.061321900643115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e9573aaa61cb239%3A0x26effa57f39eff0f!2sA%20Really%20Good%20Therapist%20Centre!5e0!3m2!1sen!2sil!4v1676796423714!5m2!1sen!2sil"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3582.06802981943!2d28.062950073956348!3d-26.12932476092588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x1e950d1f8cc86c29%3A0xdac895ec13fe29df!2sBlubird%20Medical%20Suites!5e0!3m2!1sen!2sil!4v1683828446772!5m2!1sen!2sil"
             {...attributes}></iframe>
         </div>
       </div>
